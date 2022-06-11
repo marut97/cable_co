f9f400b371f0aabbe4a837b06fdf3f1bd17366b1
marut97 <marut1997@gmail.com>
2022-06-11 22:40:49 +0530

    Adding buttons for login

--- lib/ui/authentication.dart
+++ lib/ui/authentication.dart
@@ -8,6 +8,9 @@ class Authentication extends StatefulWidget {
 }
 
 class _AuthenticationState extends State<Authentication> {
+  TextEditingController _emailField = TextEditingController();
+  TextEditingController _passwordField = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +21,46 @@ class _AuthenticationState extends State<Authentication> {
           color: Colors.blueAccent,
         ),
         child: Column(
-          children: [],
+          children: [
+            TextFormField(
+              controller: _emailField,
+              decoration: InputDecoration(
+                  hintText: "something@email.com",
+                  hintStyle: TextStyle(color: Colors.white),
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: Colors.white)),
+            ),
+            TextFormField(
+                controller: _passwordField,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "password",
+                    hintStyle: TextStyle(color: Colors.white),
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.white))),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.white),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text("Register"),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.white),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text("Register"),
+              ),
+            ),
+          ],
         ),
       ),
     );
